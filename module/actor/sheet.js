a09b2feba7861628fdf2779880c84de97717ab2b
David <74268782+DrOgres@users.noreply.github.com>
2024-08-24 11:04:06 -0500

    player gear page inital breakout

--- module/actor/sheet.js
+++ module/actor/sheet.js
@@ -1,41 +1,71 @@
 export default class esActorSheet extends ActorSheet {
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      classes: ["es", "sheet", "actor"],
+      template: "systems/electric-state/templates/actors/player.hbs",
+      width: 600,
+      height: 600,
+      tabs: [
+        {
+          navSelector: ".sheet-tabs",
+          contentSelector: ".sheet-body",
+          initial: "main",
+        },
+      ],
+    });
+  }
 
-    static get defaultOptions() {
-        return foundry.utils.mergeObject(super.defaultOptions, {
-            classes: ["es", "sheet", "actor"],
-            template: "systems/electric-state/templates/actors/player.hbs",
-            width: 600,
-            height: 600,
-            tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "main" }]
-        });
+  get template() {
+    return `systems/electric-state/templates/actors/${this.actor.type}.hbs`;
+  }
+
+  getData() {
+    const data = super.getData();
+    const actor = this.actor;
+    console.log("E-STATE | Actor", actor);
+    console.log("E-STATE |  Data", data);
+
+    this.computeItems(data);
+
+    return data;
+  }
+
+  activateListeners(html) {
+    console.log("E-STATE | Activating Actor Sheet Listeners");
+    super.activateListeners(html);
+    // html.find(".item-create").click(this._onItemCreate.bind(this));
+    // html.find(".item-edit").click(this._onItemEdit.bind(this));
+    // html.find(".item-delete").click(this._onItemDelete.bind(this));
+    // html.find(".item-roll").click(this._onItemRoll.bind(this));
+  }
+
+  //   "gear",
+  //   "weapon",
+  //   "armor",
+  //   "explosive",
+  //   "talent",
+  //   "trait",
+  //   "drone",
+  //   "neurocaster",
+  //   "tension",
+  //   "trauma",
+  //   "injury"
+
+  computeItems(data) {
+    console.log("E-STATE | Computing Items", data);
+    for (let item of Object.values(data.items)) {
+      console.log("E-STATE | Item", item);
+      item.isGear = item.type === "gear";
+      item.isWeapon = item.type === "weapon";
+      item.isArmor = item.type === "armor";
+      item.isExplosive = item.type === "explosive";
+      item.isTalent = item.type === "talent";
+      item.isTrait = item.type === "trait";
+      item.isDrone = item.type === "drone";
+      item.isNeurocaster = item.type === "neurocaster";
+      item.isTension = item.type === "tension";
+      item.isTrauma = item.type === "trauma";
+      item.isInjury = item.type === "injury";
     }
-
-    get template() {
-        return `systems/electric-state/templates/actors/${this.actor.type}.hbs`;
-      }
-
-      getData() {
-        const data = super.getData();
-        const actor = this.actor;
-        console.log("E-STATE | Actor", actor);
-        console.log("E-STATE |  Data", data);
-//TODO break out item types into separate arrays
-
-        return data;
-      }
-
-      activateListeners(html) {
-        console.log("E-STATE | Activating Actor Sheet Listeners");
-        super.activateListeners(html);
-        // html.find(".item-create").click(this._onItemCreate.bind(this));
-        // html.find(".item-edit").click(this._onItemEdit.bind(this));
-        // html.find(".item-delete").click(this._onItemDelete.bind(this));
-        // html.find(".item-roll").click(this._onItemRoll.bind(this));
-      }
-
-
-
-
-
-
+  }
 }
--- module/item/sheet.js
+++ module/item/sheet.js
@@ -1,41 +1,41 @@
-
 import { eState } from "../config.js";
 
 export default class esItemSheet extends ItemSheet {
-    constructor(...args) {
-        super(...args);
-    }
+  constructor(...args) {
+    super(...args);
+  }
+
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      width: 650,
+      height: 350,
+      classes: ["es", "sheet", "item"],
+      resizable: true,
+    });
+  }
+
+  get template() {
+    return `systems/electric-state/templates/items/${this.item.type}.hbs`;
+  }
+
+  getData() {
+ 
+    const data = super.getData();
+    data.config = eState;
+    const attributesAbv = data.config.attributesAbv;
+    const localizedAttributes = attributesAbv.map((a) =>
+      game.i18n.localize(`estate.ATTRIBUTE.${a}`)
+    );
+
+    data.config.attributeSelectOptions = {
+      STR: localizedAttributes[0],
+      AGI: localizedAttributes[1],
+      WIT: localizedAttributes[2],
+      EMP: localizedAttributes[3],
+    };
+    data.config.default = "STR";
 
 
-    static get defaultOptions() {
-        return foundry.utils.mergeObject(super.defaultOptions, {
-            width : 650, 
-            height: 350,
-            classes: ["es", "sheet", "item"],
-            resizable: true, 
-        });
-    }
-
-
-    get template() {
-        return `systems/electric-state/templates/items/${this.item.type}.hbs`;
-    }
-
-
-    getData() {
-        console.log("E-STATE | Item Data", eState);
-        const data = super.getData();
-        data.config = eState;
-        const attributes = data.config.attributes;
-        console.log(attributes);
-        const attributesAbv = data.config.attributesAbv;
-        console.log(attributesAbv);
-        const localizedAttributes = attributesAbv.map(a => game.i18n.localize(`estate.ATTRIBUTE.${a}`));
-        console.log(localizedAttributes);
-
-        data.config.attributeSelectOptions = {"STR": localizedAttributes[0], "AGI": localizedAttributes[1], "WIT": localizedAttributes[2], "EMP": localizedAttributes[3]};
-        console.log(data.config.attributeSelectOptions);
-        data.config.default = "STR";
-        return data;
-    }
+    return data;
+  }
 }

df924a7e66687ed79d417f1bf1f13ab9a746e559
David <74268782+DrOgres@users.noreply.github.com>
2024-10-07 08:38:29 -0500

    changed some fields to text area and styled them to fit

--- module/actor/sheet.js
+++ module/actor/sheet.js
@@ -6,7 +6,7 @@ export default class esActorSheet extends ActorSheet {
     return foundry.utils.mergeObject(super.defaultOptions, {
       classes: ["es", "sheet", "actor"],
       width: 650,
-      height: 700,
+      height: 'fit-content',
       tabs: [
         {
           navSelector: ".sheet-tabs",
